5acebcf58792b64f912a0b4ffda4845266810116
Terry PythonEnthusiastic <terry.zambrano2508@stu.mtec.edu>
2023-07-10 22:53:28 -0600

    Added display for game details

--- src/app/components/game-details/game-details.component.ts
+++ src/app/components/game-details/game-details.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { switchMap, tap } from 'rxjs';
+import { ListType } from 'src/app/enums/list-type';
 import { Game } from 'src/app/interfaces/game';
 import { GameService } from 'src/app/services/game.service';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-game-details',
@@ -11,10 +14,13 @@ import { GameService } from 'src/app/services/game.service';
 })
 export class GameDetailsComponent implements OnInit {
   game: Game;
+  ownedGames: {[id: string]: Game} = {};
+  wishListGames: {[id: string]: Game} = {};
 
   constructor(
     private gameService: GameService,
     private route: ActivatedRoute,
+    private localStorageService: LocalStorageService,
   ) { }
 
   ngOnInit(): void {
@@ -24,5 +30,11 @@ export class GameDetailsComponent implements OnInit {
       }),
       tap(game => this.game = game[0])
     ).subscribe()
+
+    this.localStorageService.getGameList(ListType.OWNEDLIST);
+    this.localStorageService.getGameList(ListType.WISHLIST);
+
+    this.localStorageService.ownedGames.subscribe(games => this.ownedGames = _.mapKeys(games, 'id'));
+    this.localStorageService.wishListGames.subscribe(games => this.wishListGames = _.mapKeys(games, 'id'));
   }
 }
