0bbaf5dee0096318a9abcd696bad6c854cf78d6d
Terry PythonEnthusiastic <terry.zambrano2508@stu.mtec.edu>
2023-07-10 22:39:04 -0600

    Added param retrieval

--- src/app/components/game-details/game-details.component.ts
+++ src/app/components/game-details/game-details.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs';
+import { Game } from 'src/app/interfaces/game';
+import { GameService } from 'src/app/services/game.service';
 
 @Component({
   selector: 'app-game-details',
   templateUrl: './game-details.component.html',
   styleUrls: ['./game-details.component.scss']
 })
-export class GameDetailsComponent {
+export class GameDetailsComponent implements OnInit {
+  game: Game;
 
+  constructor(
+    private gameService: GameService,
+    private route: ActivatedRoute,
+  ) { }
+
+  ngOnInit(): void {
+    this.route.params.pipe(
+      switchMap(params => {
+        return this.gameService.getById(params['gameId'])
+      }),
+      tap(game => this.game = game[0])
+    ).subscribe()
+  }
 }
