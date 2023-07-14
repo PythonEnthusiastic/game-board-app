0e9f93ea058681bfaa75e6212327bcb40abb21b0
Terry PythonEnthusiastic <terry.zambrano2508@stu.mtec.edu>
2023-07-13 19:38:24 -0600

    Added wishlist aand owned inside dynamic pages, and also added functionality to wishlist and owned pages

--- src/app/components/user-selections/user-selections.component.ts
+++ src/app/components/user-selections/user-selections.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ListType } from 'src/app/enums/list-type';
+import { Game } from 'src/app/interfaces/game';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
 
 @Component({
   selector: 'app-user-selections',
   templateUrl: './user-selections.component.html',
   styleUrls: ['./user-selections.component.scss']
 })
-export class UserSelectionsComponent {
+export class UserSelectionsComponent implements OnInit {
+  listType: ListType;
+  listTypeTitle: string;
+  ownedGames: Game[];
+  wishListGames: Game[];
+  
+  constructor(
+    private route: ActivatedRoute,
+    private localStorageService: LocalStorageService
+  ) { }
+
+  ngOnInit(): void {
+    this.listType = this.route.snapshot.routeConfig.path === 'owned' ? ListType.OWNEDLIST : ListType.WISHLIST;
+    this.listTypeTitle = this.listType === ListType.OWNEDLIST ? 'Owned Games' : 'Wishlist Games';
+  }
 
 }
